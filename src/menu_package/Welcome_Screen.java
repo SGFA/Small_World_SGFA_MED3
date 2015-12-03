cade78dc847ed9687b3fb29443b2cf3d82a42eed
Romaris <raminta.stanulionyte@gmail.com>
2015-12-03 22:29:55 +0100

    Buttons from welcome screen fixed

--- src/menu_package/Welcome_Screen.java
+++ src/menu_package/Welcome_Screen.java
@@ -10,46 +10,45 @@ public class Welcome_Screen extends BasicGameState {
 	private Image play_img;
 	private Image exit_img;
 	private Image background_img;
+	
+	Button play_btn;
+	Button exit_btn;
 
 	public Welcome_Screen(int state) {
 
 	}
 
-	MouseOverArea moa_play;
-	MouseOverArea moa_exit;
 
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		background_img = new Image("res/small world.png");
 
-		play_img = new Image("res/Play now button.png");
-		moa_play = new MouseOverArea(gc, play_img, 180, 270);
-
-		exit_img = new Image("res/exit.png");
-		moa_exit = new MouseOverArea(gc, play_img, 320, 440);
+		play_btn = new Button(gc, new Image("res/Play now button.png"), 180, 270 );
+		
+		exit_btn = new Button(gc,new Image("res/exit.png"), 320, 440 );
+		
 
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		background_img.draw(0, 0, 800, 600);
-		play_img.draw(180, 270);
-		exit_img.draw(320, 440);
+		play_btn.display();
+		exit_btn.display();
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
 
-		if (moa_play.isMouseOver()) {
-			if (gc.getInput().isMousePressed(0)) {
+		if (play_btn.isPressed()) {
+			
 					sbg.enterState(1);
-			}
+			
 		}
 
-		if (moa_exit.isMouseOver()) {
-			if (gc.getInput().isMousePressed(0)) {
+		if (exit_btn.isPressed()) {
+			
 					System.exit(0);
-			}
 		}
 	}
 
